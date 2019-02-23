a21946eba07f9308401967c77dc2de51ab3e94ec
Sleh Mastouri <slaheddine.mastouri@esprit.tn>
2019-02-23 14:51:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path')
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+var db = require('./helper/db.js')
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('view engine', 'html');
+
+const BASE_URL = '/api'
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit:'10gb' }));
+// app.use(history())
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(BASE_URL + '/', index);
+app.use(BASE_URL + '/users', users);
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'), (err) => {
+    if (err) res.status(500).send(err)
+  })
+})
+
+//Manually enable cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS,HEAD")
+  next();
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
---
+++ helper/db.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql')
+  , async = require('async')
+
+var PRODUCTION_DB = 'vidovii_database'
+  , TEST_DB = 'taktak'
+
+exports.MODE_TEST = 'mode_test'
+exports.MODE_PRODUCTION = 'mode_production'
+
+var state = {
+  pool: null,
+  mode: null,
+}
+
+exports.connect = function(mode, done) {
+  state.pool = mysql.createPool({
+    host: mode === exports.MODE_PRODUCTION ? '41.231.123.60' : 'localhost',
+    user: mode === exports.MODE_PRODUCTION ? 'vidovii' : 'root',
+    password: mode === exports.MODE_PRODUCTION ? 'wKe)xnDRHwb9' : '',
+    database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
+  })
+
+  state.mode = mode
+  done()
+}
+
+exports.get = function() {
+  return state.pool
+}
+
+exports.fixtures = function(data) {
+  var pool = state.pool
+  if (!pool) return done(new Error('Missing database connection.'))
+
+  var names = Object.keys(data.tables)
+  async.each(names, function(name, cb) {
+    async.each(data.tables[name], function(row, cb) {
+      var keys = Object.keys(row)
+        , values = keys.map(function(key) { return "'" + row[key] + "'" })
+
+      pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
+    }, cb)
+  }, done)
+}
+
+exports.persistArray = function (tableName, array) {
+  var pool = state.pool
+  if (!pool) return done(new Error('Missing database connection.'))
+
+  async.each(array, function (element ,cb) {
+    var keys = Object.keys(v)
+    var values = keys.map(function (key) { return "'" + v[key] + "'" })
+    pool.query('INSERT INTO ' + tableName + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
+  }, done)
+}
+
+exports.drop = function(tables, done) {
+  var pool = state.pool
+  if (!pool) return done(new Error('Missing database connection.'))
+
+  async.each(tables, function(name, cb) {
+    pool.query('DELETE * FROM ' + name, cb)
+  }, done)
+}
+
